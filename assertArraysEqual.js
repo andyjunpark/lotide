89c86793a9236a6273046072866667b7143cc971
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-24 02:04:18 +0000

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr2[i]) {
+      return false
+    }
+  } return true
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log(`❌❌❌Assertion Arrays Failed: ${arr1} !== ${arr2}`);
+  } else if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅Assertion Arrays Passed: ${arr1} === ${arr2}`);
+  }
+};
