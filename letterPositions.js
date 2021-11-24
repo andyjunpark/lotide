f70efc07bf28dc2e5de4c8049188dcccae9bfd4c
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-24 23:38:22 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+// TEST/ASSERTION FUNCTIONS
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log(`❌❌❌Assertion Arrays Failed: ${arr1} !== ${arr2}`);
+  } else if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅Assertion Arrays Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  let splitSentence = sentence.split("")
+  console.log(splitSentence);
+  for (let i = 0; i < splitSentence.length; i++) {
+    if (!results[splitSentence[i]]) {
+      results[splitSentence[i]] = [i]
+    } else {
+      results[splitSentence[i]].push(i)
+    }
+  } return results;
+};
+
+// TEST CODE
+// assertArraysEqual(letterPositions("hello").e, [1]);
+
+console.log(letterPositions('hello'));
+console.log(letterPositions('heeeee'));
+
