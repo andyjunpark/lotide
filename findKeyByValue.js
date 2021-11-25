7ca4b89ccf7d9925ae55069b5ef82c4e289d759d
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-25 01:43:53 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// TEST/ASSERTION FUNCTIONS
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const findKeyByValue = function(obj, name){
+  for (const item of Object.keys(obj)) {
+    if (obj[item] === name) {
+      return item
+    }
+  }
+}
+
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+bestTVShowsByGenre.romance = "The Vow"
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Vow"), "romance");
---
+++ test.js
@@ -0,0 +1,8 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(Object.keys(bestTVShowsByGenre))
+console.log(bestTVShowsByGenre."The Wire")
