f3c69722463d43afc7ef269bd0e8f52dea77b227
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-25 01:45:20 +0000

    added eqObjects.js, deleted test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-console.log(Object.keys(bestTVShowsByGenre))
-console.log(bestTVShowsByGenre."The Wire")
