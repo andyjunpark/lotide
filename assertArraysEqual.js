ceee31b8bd59e014e3046436ca73060d9eed9ce2
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-29 22:27:00 +0000

    Refactoring assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === false) {
@@ -13,3 +7,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`✅✅✅Assertion Arrays Passed: ${arr1} === ${arr2}`);
   }
 };
+
+module.exports = assertArraysEqual;
