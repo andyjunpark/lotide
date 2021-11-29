8f40f5f3edccfeb6262433a93b724be49a86b351
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-29 22:14:56 +0000

    Refactoring assertEqual.js && head.js

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('Hi', 'Hi');
-assertEqual('Hi', 'Hello');
-assertEqual(2, 2);
-assertEqual(12, 13);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('Hi', 'Hi');
+assertEqual('Hi', 'Hello');
+assertEqual(2, 2);
+assertEqual(12, 13);
