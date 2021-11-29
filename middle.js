03157c54eff0d2f84c1acff836f7e158de298321
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-11-29 22:47:52 +0000

    incorporate Mocha && Chai

--- middle.js
+++ middle.js
@@ -17,11 +17,3 @@ const middle = function(arr) {
 };
 
 module.exports = middle;
-// TEST CODE
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(middle([1]))
-console.log(middle([1, 2]))
-const words = ["hello", "world", "lighthouse"];
-assertArraysEqual(middle(words),["world"]);
-assertArraysEqual(middle(["hello", "world", "lighthouse"]),["world"]);
